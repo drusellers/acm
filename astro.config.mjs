476a3a7f13bc79be85d24dd3629d5c81b9629c86
Dru Sellers <dru@drusellers.com>
2025-02-24 07:19:20 -0600

    Upgrade to Tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), tailwind(), react()],
+  integrations: [mdx(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
